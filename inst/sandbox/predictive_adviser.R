98833b3a47c22526efa4abd375a37831e78be3bd
Ramnath Vaidyanathan <ramnath@ramnaths-air.attlocal.net>
2017-03-31 14:20:40 -0700

    add datadoctor.csv and update paths.

--- inst/sandbox/predictive_adviser.R
+++ inst/sandbox/predictive_adviser.R
@@ -2,7 +2,7 @@ library(flightdeck)
 library(htmltools)
 
 
-message = "This feature has `95% fitness`, but `0% believability`. The low believability is driven by a bivariate importance weight of 1, which indicates that it is too good a predictor to be true. 
+message = "This feature has `95% fitness`, but `0% believability`. The low believability is driven by a bivariate importance weight of 1, which indicates that it is too good a predictor to be true.
 
 Please answer the following questions to help decide how
 we should handle this feature:
@@ -30,20 +30,20 @@ fdCheckbox <- function(x){
 message <- tags$div(
   tags$p(class = 'lead', HTML(commonmark::markdown_html(message_data$main))),
   tagList(lapply(message_data$questions, fdCheckbox))
-) 
+)
 
-d <- read.csv('~/Desktop/DOWNLOADS/datadoctor.csv')
+d <- read.csv('datadoctor.csv')
 
 # Actions:
 # Inspect /Fix /Drop /None
-# 
+#
 
 d1 <- fdSimpleTable(d)
 
 d$Fitness <- d$Fitness*100
 d$Believability <- d$Believability*100
 d$Action <- "<span class='label bg-green'>Keep</span>"
-d$Action = ifelse(d$Fitness == 0 | (!is.na(d$Percentage.Uniques) & d$Percentage.Uniques == 100), 
+d$Action = ifelse(d$Fitness == 0 | (!is.na(d$Percentage.Uniques) & d$Percentage.Uniques == 100),
   "<span class='label bg-orange modal-click'>Inspect</span>",
   "<span class='label bg-green'>Keep</span>"
 )
@@ -54,35 +54,35 @@ d <- cbind(
 )
 
 library(DT)
-table1 <- datatable(d, 
-  rownames = FALSE, 
-  extensions = c("Buttons", "Responsive"), 
-  options = list(), 
-  style = "bootstrap", 
-  width = "100%", 
-  height = if (NROW(d) > 10) 550 else NULL, 
-  class = c("stripe", "hover", "cell-border"), 
+table1 <- datatable(d,
+  rownames = FALSE,
+  extensions = c("Buttons", "Responsive"),
+  options = list(),
+  style = "bootstrap",
+  width = "100%",
+  height = if (NROW(d) > 10) 550 else NULL,
+  class = c("stripe", "hover", "cell-border"),
   escape = FALSE
 ) %>%
-  formatStyle("Percentage.Missing", 
+  formatStyle("Percentage.Missing",
     background = styleColorBar(c(0, 100), 'steelblue'),
-    backgroundSize = "98% 88%", 
-    backgroundRepeat = "no-repeat", 
-    backgroundPosition = "center", 
+    backgroundSize = "98% 88%",
+    backgroundRepeat = "no-repeat",
+    backgroundPosition = "center",
     color = "transparent"
   ) %>%
-  formatStyle("Percentage.Uniques", 
+  formatStyle("Percentage.Uniques",
    background = styleColorBar(c(0, 100), 'steelblue'),
-   backgroundSize = "98% 88%", 
-   backgroundRepeat = "no-repeat", 
-   backgroundPosition = "center", 
+   backgroundSize = "98% 88%",
+   backgroundRepeat = "no-repeat",
+   backgroundPosition = "center",
    color = "transparent"
   )
-  
+
 
 # fdBoard(
-#   fdHeader(title = 'DataDoctor'), 
-#   fdSidebar(), 
+#   fdHeader(title = 'DataDoctor'),
+#   fdSidebar(),
 #   fdBody(
 #     fdRowBox(width = 12, table1),
 #     tags$script(HTML(sprintf("
@@ -95,7 +95,7 @@ table1 <- datatable(d,
 #      jsonlite::toJSON(list(
 #         title = as.character(
 #           span('Explanation', span(class = 'label label-warning', 'INSPECT'))
-#         ), 
+#         ),
 #         message = as.character(message)
 #     ), auto_unbox = TRUE)
 #     )))
@@ -103,12 +103,12 @@ table1 <- datatable(d,
 # )
 
 scr1 <- tags$script(HTML(
-  paste(readLines('~/Desktop/DOWNLOADS/trigger-modal.js', warn = F), collapse ='\n'))
+  paste(readLines('trigger-modal.js', warn = F), collapse ='\n'))
 )
-      
+
 fdBoard(
-  fdHeader(title = 'DataDoctor'), 
-  fdSidebar(), 
+  fdHeader(title = 'DataDoctor'),
+  fdSidebar(),
   fdBody(
     fdRowBox(width = 12, table1), scr1
   )
